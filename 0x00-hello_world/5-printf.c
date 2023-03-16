12138d3ddff3e171b1571756028405d7caf92ade
Ebenezerkifle <ebenikifle123@gmail.com>
2023-03-16 02:49:38 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
-* main - entry point
+*
+* main -entry point
 *
 * Return: 0 number
 */
 int main(void)
 {
-printf(with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammer, but the outcome is a piece of art, \n");
 return (0);
 }
