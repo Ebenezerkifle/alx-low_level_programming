9e4743090b466e9992b45da7d93da24b055e43c4
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-03-24 11:44:15 +0300

    more functions added

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+/*
+ * File: 6-print_line.c
+ * Auth: Abeniezer Kifle
+ */
+
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+    int len;
+
+    if (n > 0)
+    {
+        for (len = 0; len < n; len++)
+            _putchar('_');
+    }
+
+    _putchar('\n');
+}
