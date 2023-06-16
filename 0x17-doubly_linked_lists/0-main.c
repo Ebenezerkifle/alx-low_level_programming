37e952722b061d0719055d4cc0557f8ea8743a4e
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-06-16 19:29:10 +0300

    lists.h file is updated

--- 0x17-doubly_linked_lists/0-main.c
+++ 0x17-doubly_linked_lists/0-main.c
@@ -11,24 +11,96 @@
 int main(void)
 {
     dlistint_t *head;
-    dlistint_t *new;
-    dlistint_t hello = {8, NULL, NULL};
-    size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        dprintf(2, "Error: Can't malloc\n");
-        return (EXIT_FAILURE);
-    }
-    new->n = 9;
-    head->prev = new;
-    new->next = head;
-    new->prev = NULL;
-    head = new;
-    n = print_dlistint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    free_dlistint(head);
     return (EXIT_SUCCESS);
+}
+void free_dlistint(dlistint_t *head)
+{
+    if (head == NULL)
+        return;
+    free_dlistint(head->next);
+    free(head);
+}
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t nodes = 0;
+
+    if (h == NULL)
+        return (0);
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        nodes++;
+    }
+
+    return (nodes);
+}
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    dlistint_t *new;
+
+    if (!head)
+        return (NULL);
+
+    new = malloc(sizeof(dlistint_t));
+    if (!new)
+        return (NULL);
+
+    new->n = n;
+
+    new->next = *head;
+    new->prev = NULL;
+
+    if (*head)
+        (*head)->prev = new;
+
+    *head = new;
+
+    return (new);
+}
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+    dlistint_t *new;
+    dlistint_t *temp = *head;
+
+    if (!head)
+        return (NULL);
+
+    new = malloc(sizeof(dlistint_t));
+    if (!new)
+        return (NULL);
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+    {
+        new->prev = NULL;
+        *head = new;
+        return (new);
+    }
+
+    while (temp->next)
+        temp = temp->next;
+
+    temp->next = new;
+    new->prev = temp;
+
+    return (new);
 }
